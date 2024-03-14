cfaf78e1e8fc2c00b9667f16cab656c9e341d04d
Bailey Matthews <bailey@hey.com>
2024-03-14 11:55:44 +0000

    chore: remove database init from main

--- main.go
+++ main.go
@@ -4,16 +4,15 @@ import (
 	"fmt"
 
 	"github.com/baileyjm02/muzz-backend-technical-exercise/api"
-	"github.com/baileyjm02/muzz-backend-technical-exercise/database"
+	"github.com/baileyjm02/muzz-backend-technical-exercise/server"
 	"github.com/baileyjm02/muzz-backend-technical-exercise/utils"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
 
-	database.Start()
-
-	api.Start()
+	go server.Start()
+	go api.Start()
 
 	stateCheck := make(chan struct{})
 	channels := []chan struct{}{
