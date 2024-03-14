3a87543943dbcadf1c650b00fb82a5fed0178343
Bailey Matthews <bailey@hey.com>
2024-03-14 13:06:53 +0000

    feat: add server

---
+++ server/server.go
@@ -0,0 +1,25 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+// Start initiates the HTTP server
+func Start() {
+	// Start the HTTP server ()
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      GetRouter(),
+		ReadTimeout:  6 * time.Second,
+		WriteTimeout: 6 * time.Second,
+	}
+
+	// Inform us that we are starting the server
+	fmt.Printf("Starting server on %v\n", server.Addr)
+
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Printf("Server error: %v", err)
+	}
+}
