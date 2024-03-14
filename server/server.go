7424a329ee8503a94e53a0763501128295b0099c
Bailey Matthews <bailey@hey.com>
2024-03-14 17:40:11 +0000

    docs: update comments

--- server/server.go
+++ server/server.go
@@ -8,7 +8,7 @@ import (
 
 // Start initiates the HTTP server
 func Start() {
-	// Start the HTTP server ()
+	// Declare the server
 	server := &http.Server{
 		Addr:         ":3000",
 		Handler:      GetRouter(),
@@ -19,6 +19,7 @@ func Start() {
 	// Inform us that we are starting the server
 	fmt.Printf("Starting server on %v\n", server.Addr)
 
+	// Block the thread while listening
 	if err := server.ListenAndServe(); err != nil {
 		fmt.Printf("Server error: %v", err)
 	}
